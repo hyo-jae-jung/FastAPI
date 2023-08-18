f70f1282429cdd0d4916e9795ff3926bb58bea6d
hyo-jae-jung <hyojaejung@kakao.com>
2023-08-18 13:13:37 +0900

    CRUD with mongoDB

--- planner/main.py
+++ planner/main.py
@@ -1,14 +1,20 @@
 from fastapi import FastAPI
 from routes.users import user_router
 from routes.events import event_router
+from database.connection import Settings 
 
 import uvicorn
 # from fastapi.responses import RedirectResponse
 app = FastAPI()
+settings = Settings()
 
 # Register routes
 app.include_router(user_router,  prefix="/user")
 app.include_router(event_router, prefix="/event")
+
+@app.on_event("startup")
+async def init_db():
+    await settings.initialize_database()
 
 # @app.get("/")
 # async def home():
--- planner/models/users.py
+++ planner/models/users.py
@@ -1,19 +1,23 @@
-from pydantic import BaseModel, EmailStr
 from typing import Optional, List
+from beanie import Document, Link
+from pydantic import BaseModel, EmailStr
 from models.events import Event
 
 
-class User(BaseModel):
+class User(Document):
     email: EmailStr
     password: str
     events: Optional[List[Event]]
-
+    
+    class Settings:
+        name = "users"
+    
     class Config:
         schema_extra = {
             "example": {
                 "email": "fastapi@packt.com",
                 "password": "strong!!!",
-                "events": [],
+                "events":[]
             }
         }
 
@@ -22,14 +26,6 @@ class UserSignIn(BaseModel):
     email: EmailStr
     password: str
     
-    class Config:
-        schema_extra = {
-            "example": {
-                "email": "fastapi@packt.com",
-                "password": "strong!!!",
-                "events": [],
-            }
-        }
 
 if __name__ == "__main__":
     
@@ -37,12 +33,11 @@ if __name__ == "__main__":
                 "email": "fastapi@packt.com",
                 "password": "strong!!!",
                 "events": [{   
-                "id": 1,
                 "title": "FastAPI Book Launch",
                 "image": "https://linktomyimage.com/image.png",
                 "description": "We will be discussing the contents of the FastAPI book in this event.Ensure to come with your own copy to win gifts!",
                 "tags": ["python", "fastapi", "book", "launch"],
-                # "location": "Google Meet"
+                "location": "Google Meet"
                 }],
             }
     
